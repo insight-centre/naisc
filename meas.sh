b60d9d8070d00e9f1aedb6b3f6087fa46c772d03
John P. McCrae <john@mccr.ae>
2019-05-09 10:10:03 +0100

    Meas script also builds Naisc Core

--- meas.sh
+++ meas.sh
@@ -2,6 +2,7 @@
 
 if [ ! -f naisc-meas/target/naisc-meas-jar-with-dependencies.jar ]
 then
+    mvn -q -f naisc-core/pom.xml install
     mvn -q -f naisc-meas/pom.xml package assembly:single
 fi
 java -jar naisc-meas/target/naisc-meas-jar-with-dependencies.jar "$@"
