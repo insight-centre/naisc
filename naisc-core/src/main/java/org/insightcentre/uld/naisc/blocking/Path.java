46e3a9805807bcb2d843f3cfaa885aa0ec6926e3
John P. McCrae <john@mccr.ae>
2019-07-30 16:48:15 +0100

    Implement Path based blocking strategy

---
+++ naisc-core/src/main/java/org/insightcentre/uld/naisc/blocking/Path.java
@@ -0,0 +1,210 @@
+package org.insightcentre.uld.naisc.blocking;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.NoSuchElementException;
+import java.util.Set;
+import java.util.stream.Collectors;
+import org.apache.jena.rdf.model.ResIterator;
+import org.apache.jena.rdf.model.Resource;
+import org.apache.jena.rdf.model.Statement;
+import org.apache.jena.rdf.model.StmtIterator;
+import org.insightcentre.uld.naisc.BlockingStrategy;
+import org.insightcentre.uld.naisc.BlockingStrategyFactory;
+import org.insightcentre.uld.naisc.ConfigurationParameter;
+import org.insightcentre.uld.naisc.Dataset;
+import org.insightcentre.uld.naisc.NaiscListener;
+import org.insightcentre.uld.naisc.analysis.Analysis;
+import org.insightcentre.uld.naisc.lens.Label;
+import org.insightcentre.uld.naisc.util.Lazy;
+import org.insightcentre.uld.naisc.util.Pair;
+
+/**
+ * Blocking strategy that uses the path distance to decide if two entities are
+ * related
+ *
+ * @author John McCrae
+ */
+public class Path implements BlockingStrategyFactory {
+
+    @Override
+    public BlockingStrategy makeBlockingStrategy(Map<String, Object> params, Lazy<Analysis> analysis, NaiscListener listener) {
+        Configuration config = new ObjectMapper().convertValue(params, Configuration.class);
+        if (config.maxMatches <= 0) {
+            throw new IllegalArgumentException("Must have at least one match");
+        }
+        if (config.preblockLeftProperty == null) {
+            throw new IllegalArgumentException("Property for preblocking cannot be null");
+        }
+        return new PathImpl(new Preblocking(Collections.singleton(new Pair<>(config.preblockLeftProperty,
+                config.preblockRightProperty == null || config.preblockRightProperty.equals("") ? config.preblockLeftProperty : config.preblockRightProperty))), config.maxMatches);
+    }
+
+    public static class Configuration {
+
+        /**
+         * The maximum number of nodes to explore in the path method
+         */
+        @ConfigurationParameter(description="The maximum number of nodes to explore in the path method")
+        public int maxMatches = 100;
+
+        /**
+         * The property to use in the left side of the pre-blocking
+         */
+        @ConfigurationParameter(description = "The property to use in the left side of the pre-blocking")
+        public String preblockLeftProperty = Label.RDFS_LABEL;
+
+        /**
+         * The property to use in the right side of the pre-blocking (or empty for same as left)
+         */
+        @ConfigurationParameter(description = "The property to use in the right side of the pre-blocking (or empty for same as left)")
+        public String preblockRightProperty = null;
+    }
+
+    private static class PathImpl implements BlockingStrategy {
+
+        private final Preblocking preblocking;
+        private final int maxMatches;
+
+        public PathImpl(Preblocking preblocking, int maxMatches) {
+            this.preblocking = preblocking;
+            this.maxMatches = maxMatches;
+        }
+
+        @Override
+        public Iterable<Pair<Resource, Resource>> block(Dataset left, Dataset right, NaiscListener log) {
+            Map<Resource, List<Resource>> prelinking = convertPrelinking(preblocking.preblock(left, right, log));
+            return new Iterable<Pair<Resource, Resource>>() {
+                @Override
+                public Iterator<Pair<Resource, Resource>> iterator() {
+                    ResIterator leftIter = left.listSubjects();
+                    return new PathIterator(leftIter, left, right, prelinking, maxMatches);
+                }
+
+            };
+        }
+
+        private Map<Resource, List<Resource>> convertPrelinking(Set<Pair<Resource, Resource>> prelinking) {
+            Map<Resource, List<Resource>> map = new HashMap<>();
+            for (Pair<Resource, Resource> a : prelinking) {
+                if (!map.containsKey(a._1)) {
+                    map.put(a._1, new ArrayList<>());
+                }
+                map.get(a._1).add(a._2);
+            }
+            return map;
+        }
+
+    }
+
+    private static class PathIterator implements Iterator<Pair<Resource, Resource>> {
+
+        private final ResIterator leftIter;
+        private final Dataset left, right;
+        private final Map<Resource, List<Resource>> prelinking;
+        private Iterator<Pair<Resource, Resource>> nodesIter = null;
+        private final int maxMatches;
+
+        public PathIterator(ResIterator leftIter, Dataset left, Dataset right, Map<Resource, List<Resource>> prelinking, int maxMatches) {
+            this.leftIter = leftIter;
+            this.left = left;
+            this.right = right;
+            this.prelinking = prelinking;
+            this.maxMatches = maxMatches;
+            findNext();
+        }
+
+        private void findNext() {
+            while (leftIter.hasNext()) {
+                Resource l = leftIter.next();
+                if(!l.isURIResource())
+                    continue;
+                List<Pair<Resource, Resource>> nodes = new ArrayList<>();
+                Set<Resource> leftToExpand = new HashSet<>();
+                Set<Resource> leftVisited = new HashSet<>();
+                Set<Resource> rightToExpand = new HashSet<>();
+                Set<Resource> rightVisited = new HashSet<>();
+                leftToExpand.add(l);
+                while ((!leftToExpand.isEmpty() || !rightToExpand.isEmpty()) && nodes.size() < maxMatches) {
+                    Set<Resource> lte2 = new HashSet<>(), rte2 = new HashSet<>();
+                    for (Resource r : leftToExpand) {
+                        StmtIterator stIter = left.listStatements(r, null, null);
+                        while (stIter.hasNext()) {
+                            Statement s = stIter.next();
+                            if (s.getObject().isResource() && !leftVisited.contains(s.getObject().asResource())) {
+                                lte2.add(s.getObject().asResource());
+                            }
+                        }
+                        stIter = left.listStatements(null, null, r);
+                        while (stIter.hasNext()) {
+                            Statement s = stIter.next();
+                            if (!leftVisited.contains(s.getSubject())) {
+                                lte2.add(s.getSubject());
+                            }
+                        }
+                        if (prelinking.containsKey(r)) {
+                            nodes.addAll(prelinking.get(r).stream().map(r2 -> new Pair<>(l, r2)).collect(Collectors.toList()));
+                            rte2.addAll(prelinking.get(r));
+                        }
+                    }
+                    for (Resource r : rightToExpand) {
+                        StmtIterator stIter = right.listStatements(r, null, null);
+                        while (stIter.hasNext()) {
+                            Statement s = stIter.next();
+                            if (s.getObject().isResource() && !rightVisited.contains(s.getObject().asResource())) {
+                                rte2.add(s.getObject().asResource());
+                                nodes.add(new Pair<>(l, s.getObject().asResource()));
+                            }
+                        }stIter = right.listStatements(null, null, r);
+                        while (stIter.hasNext()) {
+                            Statement s = stIter.next();
+                            if (!rightVisited.contains(s.getSubject())) {
+                                rte2.add(s.getSubject());
+                                nodes.add(new Pair<>(l, s.getSubject()));
+                            }
+                        }
+                    }
+                    leftVisited.addAll(leftToExpand);
+                    rightVisited.addAll(rightToExpand);
+                    leftToExpand = lte2;
+                    rightToExpand = rte2;
+                }
+                if (!nodes.isEmpty()) {
+                    if(nodes.size() > maxMatches) 
+                        nodes.subList(maxMatches, nodes.size()).clear();
+                    nodesIter = nodes.iterator();
+                    return;
+                }
+            }
+            nodesIter = null;
+        }
+
+        @Override
+        public boolean hasNext() {
+            return nodesIter != null;
+        }
+
+        @Override
+        public Pair<Resource, Resource> next() {
+            while (nodesIter != null && !nodesIter.hasNext()) {
+                findNext();
+            }
+            if (nodesIter != null) {
+                Pair<Resource, Resource> next = nodesIter.next();
+                while (nodesIter != null && !nodesIter.hasNext()) {
+                    findNext();
+                }
+                return next;
+            } else {
+                throw new NoSuchElementException();
+            }
+        }
+
+    }
+}
---
+++ naisc-core/src/test/java/org/insightcentre/uld/naisc/blocking/PathTest.java
@@ -0,0 +1,93 @@
+package org.insightcentre.uld.naisc.blocking;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import org.apache.jena.rdf.model.AnonId;
+import org.apache.jena.rdf.model.Model;
+import org.apache.jena.rdf.model.ModelFactory;
+import org.apache.jena.rdf.model.Resource;
+import org.insightcentre.uld.naisc.Alignment;
+import org.insightcentre.uld.naisc.BlockingStrategy;
+import static org.insightcentre.uld.naisc.lens.Label.RDFS_LABEL;
+import org.insightcentre.uld.naisc.main.DefaultDatasetLoader.ModelDataset;
+import org.insightcentre.uld.naisc.main.ExecuteListeners;
+import org.insightcentre.uld.naisc.util.Lazy;
+import org.insightcentre.uld.naisc.util.Pair;
+import org.junit.After;
+import org.junit.AfterClass;
+import static org.junit.Assert.assertEquals;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ *
+ * @author John McCrae
+ */
+public class PathTest {
+
+    public PathTest() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() {
+    }
+
+    @AfterClass
+    public static void tearDownClass() {
+    }
+
+    @Before
+    public void setUp() {
+    }
+
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of makeBlockingStrategy method, of class Path.
+     */
+    @Test
+    public void testMakeBlockingStrategy() {
+        System.out.println("makeBlockingStrategy");
+        Map<String, Object> params = new HashMap<>();
+        params.put("maxMatches", "2");
+        Path instance = new Path();
+        Model left = ModelFactory.createDefaultModel();
+        Model right = ModelFactory.createDefaultModel();
+        
+        left.add(left.createStatement(left.createResource("file:foo"), left.createProperty(RDFS_LABEL), left.createLiteral("cat", "en")));
+        left.add(left.createStatement(left.createResource(new AnonId()), left.createProperty(RDFS_LABEL), left.createLiteral("dog", "en")));
+        left.add(left.createStatement(left.createResource("file:foo2"), left.createProperty(RDFS_LABEL), left.createLiteral("dog", "en")));
+        left.add(left.createStatement(left.createResource("file:foo2"), left.createProperty(Alignment.SKOS_EXACT_MATCH), left.createResource("file:foo")));
+        left.add(left.createStatement(left.createResource("file:foo3"), left.createProperty(RDFS_LABEL), left.createLiteral("cat", "ga")));
+        left.add(left.createStatement(left.createResource("file:foo3"), left.createProperty("file:foo"), left.createLiteral("cat", "en")));
+        left.add(left.createStatement(left.createResource("file:foo3"), left.createProperty(Alignment.SKOS_EXACT_MATCH), left.createResource("file:foo5")));
+        left.add(left.createStatement(left.createResource("file:foo4"), left.createProperty(RDFS_LABEL), left.createLiteral("bee", "en")));
+        
+        right.add(right.createStatement(right.createResource("file:fuzz"), right.createProperty(RDFS_LABEL), right.createLiteral("cat", "en")));
+        right.add(right.createStatement(right.createResource("file:fuzz2"), right.createProperty(RDFS_LABEL), right.createLiteral("dog", "en")));
+        right.add(right.createStatement(right.createResource("file:fuzz3"), right.createProperty(RDFS_LABEL), right.createLiteral("dog house", "en")));
+        right.add(right.createStatement(right.createResource("file:fuzz4"), right.createProperty(RDFS_LABEL), right.createLiteral("bee", "en")));
+        right.add(right.createStatement(right.createResource("file:fuzz4"), right.createProperty(Alignment.SKOS_EXACT_MATCH), right.createResource("fuzz5")));
+        right.add(right.createStatement(right.createResource("file:fuzz5"), right.createProperty(Alignment.SKOS_EXACT_MATCH), right.createResource("fuzz6")));
+        
+        BlockingStrategy strategy = instance.makeBlockingStrategy(params, Lazy.fromClosure(() -> null), ExecuteListeners.NONE);
+        Iterable<Pair<Resource,Resource>> result = strategy.block(new ModelDataset(left), new ModelDataset(right));
+        Map<Resource,List<Resource>> result2 = new HashMap<>();
+        for(Pair<Resource,Resource> r : result) {
+            if(!result2.containsKey(r._1))
+                result2.put(r._1, new ArrayList<>());
+            result2.get(r._1).add(r._2);
+        }
+        assertEquals(2, result2.get(left.createResource("file:foo")).size());
+        assertEquals(2, result2.get(left.createResource("file:foo2")).size());
+        assertEquals(2, result2.get(left.createResource("file:foo4")).size());
+        
+        
+    }
+
+}
