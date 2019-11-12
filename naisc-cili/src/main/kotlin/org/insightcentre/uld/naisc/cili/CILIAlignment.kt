3b7faba0deb2987f2868532965ea95c21fa3dcc7
John P. McCrae <john@mccr.ae>
2019-11-12 02:36:40 +0000

    Fix small bug

--- naisc-cili/src/main/kotlin/org/insightcentre/uld/naisc/cili/CILIAlignment.kt
+++ naisc-cili/src/main/kotlin/org/insightcentre/uld/naisc/cili/CILIAlignment.kt
@@ -6,6 +6,6 @@ data class CILIAlignment(val sourceId : String, val ili : Int, val score : Doubl
 
 fun naisc2cili(align : Alignment, prefix : String) = CILIAlignment(
         align.entity1?.uri?.substring(prefix.length) ?: throw RuntimeException("null entity"),
-        align.entity2?.uri?.substring(CILISQLiteDataset.ILI.length)?.toInt() ?: throw RuntimeException("null entity"),
+        align.entity2?.uri?.substring(CILISQLiteDataset.ILI.length + 1)?.toInt() ?: throw RuntimeException("null entity"),
         align.score,
         align.relation ?: throw RuntimeException("null relation"))
