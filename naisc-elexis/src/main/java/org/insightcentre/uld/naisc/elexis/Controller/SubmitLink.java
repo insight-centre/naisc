fe86989e1513d90c45802290ad983e262cd7f1b6
Sampritha Manjunath <sampritha.manjunath@insight-centre.org>
2020-02-11 19:39:06 +0000

    optimising

---
+++ naisc-elexis/src/main/java/org/insightcentre/uld/naisc/elexis/Controller/SubmitLink.java
@@ -0,0 +1,65 @@
+package org.insightcentre.uld.naisc.elexis.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+import sun.net.www.protocol.http.HttpURLConnection;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.List;
+
+@RestController
+public class SubmitLink {
+
+    @GetMapping("/dummy")
+    public String index() {
+        return "Greetings from Spring Boot!";
+    }
+
+    @GetMapping("/testConnection")
+    public String testConnection() throws IOException {
+        String apiResponse = null;
+        URL endpoint = new URL("http://localhost:8000");
+        try {
+            HttpURLConnection conn = (HttpURLConnection) endpoint.openConnection();
+            conn.setRequestMethod("GET");
+            conn.setRequestProperty("Accept", "application/json");
+
+            if (conn.getResponseCode() != 200) {
+                throw new RuntimeException("Failed : HTTP error code : " + conn.getResponseCode());
+            }
+
+            BufferedReader br = new BufferedReader(new InputStreamReader((conn.getInputStream())));
+            StringBuilder sb = new StringBuilder();
+            String line;
+
+            while ((line = br.readLine()) != null) {
+                sb.append(line+"\n");
+            }
+            br.close();
+            apiResponse =  sb.toString();
+
+            conn.disconnect();
+
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return apiResponse;
+    }
+
+    @PostMapping("/submit")
+    //@RequestMapping(value = "/submit", method = RequestMethod.POST )
+    public String submitLinkRequest() throws MalformedURLException {
+        URL endpoint = new URL("http://localhost:8000");
+        //ELEXISRest elexisRest = new ELEXISRest(endpoint);
+        //return elexisRest.getDictionaries();
+
+        return "Got it!!";
+    }
+}
---
+++ naisc-elexis/src/test/java/org/insightcentre/uld/naisc/elexis/NaiscElexisApplicationTests.java
@@ -0,0 +1,13 @@
+package org.insightcentre.uld.naisc.elexis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NaiscElexisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
