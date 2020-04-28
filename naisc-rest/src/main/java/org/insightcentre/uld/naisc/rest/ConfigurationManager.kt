8ee04a9c81692bb55620ba0cbe0773d6f7696035
John P. McCrae <john@mccr.ae>
2020-04-28 13:09:38 +0100

    Cache the results of initialization so that changes are faster

--- naisc-rest/src/main/java/org/insightcentre/uld/naisc/rest/ConfigurationManager.kt
+++ naisc-rest/src/main/java/org/insightcentre/uld/naisc/rest/ConfigurationManager.kt
@@ -24,6 +24,12 @@ object ConfigurationManager {
     private val datasets = mutableMapOf<String, Dataset>()
     private val analyses = mutableMapOf<Pair<String, String>, Lazy<Analysis>>()
     private val prelinking = mutableMapOf<Pair<String, String>, Lazy<AlignmentSet>>()
+    private val strategies = mutableMapOf<ConfigDataset, BlockingStrategy>()
+    private val lenses = mutableMapOf<ConfigDataset, List<Lens>>()
+    private val graphFeatures = mutableMapOf<ConfigDataset, List<GraphFeature>>()
+    private val textFeatures = mutableMapOf<String, List<TextFeature>>()
+    private val scorers = mutableMapOf<String, List<Scorer>>()
+    private val matchers = mutableMapOf<String, Matcher>()
 
     @Throws(InvalidConfigurationException::class)
     private fun loadConfiguration(configuration: String): Configuration {
@@ -75,41 +81,78 @@ object ConfigurationManager {
 
     @Throws(InvalidConfigurationException::class, DatasetNotFoundException::class, IOException::class)
     fun getStrategy(configuration: String, dataset1: String, dataset2: String): BlockingStrategy {
+        val strategy = strategies.get(ConfigDataset(configuration, dataset1, dataset2))
+        if(strategy != null) {
+            return strategy
+        }
         val left = getDataset(dataset1)
         val right = getDataset(dataset2)
-        return loadConfiguration(configuration).makeBlockingStrategy(getAnalysis(left, right), ExecuteListeners.STDERR)
-
+        val strategy2 = loadConfiguration(configuration).makeBlockingStrategy(getAnalysis(left, right), ExecuteListeners.STDERR)
+        strategies[ConfigDataset(configuration, dataset1, dataset2)] = strategy2
+        return strategy2
     }
 
     @Throws(InvalidConfigurationException::class, DatasetNotFoundException::class)
     fun getLens(configuration: String, dataset1: String, dataset2: String): List<Lens> {
+        val cacheLens = lenses[ConfigDataset(configuration, dataset1, dataset2)]
+        if (cacheLens != null) {
+            return cacheLens
+        }
         val left = getDataset(dataset1)
         val right = getDataset(dataset2)
         val combined = CombinedDataset(left, right)
-        return loadConfiguration(configuration).makeLenses(combined, getAnalysis(left, right), ExecuteListeners.STDERR)
+        val lens = loadConfiguration(configuration).makeLenses(combined, getAnalysis(left, right), ExecuteListeners.STDERR)
+        lenses[ConfigDataset(configuration, dataset1, dataset2)] = lens
+        return lens
     }
 
     @Throws(InvalidConfigurationException::class, DatasetNotFoundException::class)
     fun getGraphFeatures(configuration: String, dataset1 : String, dataset2 : String) : List<GraphFeature> {
+        val cacheFeatures = graphFeatures.get(ConfigDataset(configuration, dataset1, dataset2))
+        if (cacheFeatures != null) {
+            return cacheFeatures
+        }
         val left = getDataset(dataset1)
         val right = getDataset(dataset2)
         val combined = CombinedDataset(left, right)
-        return loadConfiguration(configuration).makeGraphFeatures(combined, getAnalysis(left, right), getPrelinking(left, right), ExecuteListeners.STDERR)
+        val features = loadConfiguration(configuration).makeGraphFeatures(combined, getAnalysis(left, right), getPrelinking(left, right), ExecuteListeners.STDERR)
+        graphFeatures[ConfigDataset(configuration, dataset1, dataset2)] = features
+        return features
     }
 
     @Throws(InvalidConfigurationException::class)
     fun getMatcher(configuration : String) : Matcher {
-        return loadConfiguration(configuration).makeMatcher()
+        val cacheMatcher = matchers.get(configuration)
+        if (cacheMatcher != null) {
+            return cacheMatcher
+        }
+        val matcher = loadConfiguration(configuration).makeMatcher()
+        matchers[configuration] = matcher
+        return matcher
     }
 
     @Throws(InvalidConfigurationException::class)
     fun getScorer(configuration: String) : List<Scorer> {
-        return loadConfiguration(configuration).makeScorer()
+        val cacheScorer = scorers.get(configuration)
+        if (cacheScorer != null) {
+            return cacheScorer
+        }
+        val scorer = loadConfiguration(configuration).makeScorer()
+        scorers[configuration] = scorer
+        return scorer
     }
 
     @Throws(InvalidConfigurationException::class)
     fun getTextFeatures(configuration : String) : List<TextFeature> {
-        return loadConfiguration(configuration).makeTextFeatures()
+        val cacheFeatures = textFeatures.get(configuration)
+        if (cacheFeatures != null) {
+            return cacheFeatures
+        }
+        val features = loadConfiguration(configuration).makeTextFeatures()
+        textFeatures[configuration] = features
+        return features
     }
 
 }
+
+data class ConfigDataset(val configuration : String, val dataset1 : String, val dataset2 : String)
